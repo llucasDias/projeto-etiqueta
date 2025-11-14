9ebd164a11a4230939671895bdd5297185f34f4c
Lucas Dias <luucasdias@icloud.com>
2025-11-14 14:21:07 -0300

    Initial commit - estrutura base

---
+++ src/main/java/com/lucas/projetoid/ProjetoidApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.projetoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/projetoid/ProjetoidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.projetoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
